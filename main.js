171cdfc53ec00035393257014da0db093feb211b
Kazimir84 <kav2007@ukr.net>
2022-06-02 20:18:09 +0300

    Add Home Work!

--- main.js
+++ main.js
@@ -8,7 +8,6 @@
 // ---------------------------------------------------------------------------------------------------------------------
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => {
-        console.log('Response', response)
         return response.json();
     })
     .then(posts => {
@@ -67,14 +66,88 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 //                                                Home Work
 //                                             OKTEN-HW-6-JS
 //======================================================================================================================
-//1.
+//                                                  1.
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
 // https://jsonplaceholder.typicode.com/posts
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+        return response.json();
+    })
+    .then(posts => {
+        for (let postElement of posts) {
+            document.write(`<hr>`)
+            let div = document.createElement('div');
+            document.body.append(div);
 
+            for (let postElementKey in postElement) {
+                let wrapper = document.createElement('div');
+                wrapper.className = 'wrapper';
 
-// 2.
+                let h1 = document.createElement('h1');
+                h1.className = 'h1';
+                h1.innerText = `Title: ${postElement.title}`;
+
+                let h2 = document.createElement('h2');
+                h2.innerText = `Body: ${postElement.body}`;
+
+                let h22 = document.createElement('h2');
+                h22.className = 'h22';
+                h22.innerText = `User Id: ${postElement.userId}`;
+
+                let h3 = document.createElement('h3');
+                h3.className = 'h3';
+                h3.innerText = `Id: ${postElement.id}`;
+
+                wrapper.append(h1, h22, h3, h2);
+
+                div.appendChild(wrapper);
+            };
+        };
+    })
+// ---------------------------------------------------------------------------------------------------------------------
+//                                                      2.
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
 // https://jsonplaceholder.typicode.com/comments
+fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(response => {
+        return response.json();
+    })
+    .then(comments => {
+        console.log('coments', comments)
+        for (let comment of comments) {
+            document.write(`<hr>`)
+            let div = document.createElement('div');
+            document.body.append(div);
+
+                let wrapperHW2 = document.createElement('div');
+                wrapperHW2.className = 'wrapper';
+                wrapperHW2.style.backgroundColor = 'blue';
+                document.body.append(wrapperHW2);
+
+                let h1 = document.createElement('h1');
+                h1.className = 'h1';
+                h1.innerText = `Post ID: ${comment.postId}`;
+
+                let h2 = document.createElement('h2');
+                h2.className = 'h2';
+                h2.innerText = `Id: ${comment.id}`;
+
+                let name = document.createElement('h2');
+                name.className = 'name';
+                name.innerText = `Name: ${comment.name}`;
+
+                let email = document.createElement('h2');
+                email.className = 'email';
+                email.innerText = `email: ${comment.email}`;
+
+                let body = document.createElement('p');
+                body.className = 'body';
+                body.innerText = `Body: ${comment.body}`
+
+                wrapperHW2.append(h1, h2, name, email, body);
+                div.append(wrapperHW2);
+        };
+    });
 // ---------------------------------------------------------------------------------------------------------------------
 
 
